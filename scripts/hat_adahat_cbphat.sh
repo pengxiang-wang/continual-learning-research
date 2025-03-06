94d0f1dbebb5f9eb33ddc199ee09cfabbb2c09b8
Shawn Wang <shawn.pxwang@Shawns-MacBook-Air.local>
2025-03-06 10:58:22 +0800

    global-seed

--- scripts/hat_adahat_cbphat.sh
+++ scripts/hat_adahat_cbphat.sh
@@ -1,14 +1,14 @@
 
 for seed in $(seq 1 10); 
 do
-    clrun -- experiment=til_pmnist_hat seed=$seed
-    clrun -- experiment=til_pmnist_adahat seed=$seed
-    clrun -- experiment=til_pmnist_cbphat seed=$seed
+    clrun -- experiment=til_pmnist_hat global_seed=$seed
+    clrun -- experiment=til_pmnist_adahat global_seed=$seed
+    clrun -- experiment=til_pmnist_cbphat global_seed=$seed
 done
 
 for seed in $(seq 1 10); 
 do
-    clrun -- experiment=til_scifar100_hat seed=$seed
-    clrun -- experiment=til_scifar100_adahat seed=$seed
-    clrun -- experiment=til_scifar100_cbphat seed=$seed
+    clrun -- experiment=til_scifar100_hat global_seed=$seed
+    clrun -- experiment=til_scifar100_adahat global_seed=$seed
+    clrun -- experiment=til_scifar100_cbphat global_seed=$seed
 done
