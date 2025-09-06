5ec719c78aad6231898a7b246c76a85100a41faa
Shawn Wang <shawn.pxwang@gmail.com>
2025-09-07 00:01:18 +0800

    CLArena-1.1.0

--- scripts/run_all.sh
+++
@@ -1,7 +0,0 @@
-# run all experiments in configs/experiment
-for file in ./configs/experiment/*;
-do
-    experiment_name=$(basename "$file" | sed 's/\.[^.]*$//')
-    echo "Now running: clrun -- experiment=$experiment_name"
-    clrun -- experiment=$experiment_name
-done
