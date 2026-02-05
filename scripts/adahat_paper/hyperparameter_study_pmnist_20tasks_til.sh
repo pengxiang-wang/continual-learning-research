6be128805e90ec17e0dbfcc45629124bf7fb12bc
Shawn Wang <shawn.pxwang@gmail.com>
2026-02-05 22:03:01 +0800

    fix

--- scripts/adahat_paper/hyperparameter_study_pmnist_20tasks_til.sh
+++ scripts/adahat_paper/hyperparameter_study_pmnist_20tasks_til.sh
@@ -3,6 +3,6 @@ for global_seed in {1..5}
 do
 for alpha in 1e-7 2e-7 3e-7 4e-7 5e-7 6e-7 7e-7 8e-7 9e-7 1e-6 2e-6 3e-6 4e-6 5e-6 6e-6 7e-6 8e-6 9e-6 1e-5
 do
-clarena pipeline=CL_MAIN_EXPR index=adahat_paper/adahat_pmnist_20tasks cl_paradigm=TIL output_dir=output_adahat_paper/hyperparameter_study/pmnist_20tasks_til/alpha_${alpha}/seed_${global_seed} global_seed=${global_seed} cl_algorithm.adjustment_intensity=${alpha}
+clarena pipeline=CL_MAIN_EXPR index=adahat_paper/adahat_pmnist_20tasks cl_paradigm=TIL output_dir=output_adahat_paper/hyperparameter_study/pmnist_20tasks_til/alpha_${alpha}/seed_${global_seed}/${misc.timestamp} global_seed=${global_seed} cl_algorithm.adjustment_intensity=${alpha}
 done
 done
--- scripts/fgadahat_paper/main_study_pmnist_20tasks_til.sh
+++ scripts/fgadahat_paper/main_study_pmnist_20tasks_til.sh
@@ -2,29 +2,29 @@
 for global_seed in {1..10}
 do
 # Finetuning
-clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/finetuning_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/finetuning/seed_${global_seed} global_seed=${global_seed}
+clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/finetuning_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/finetuning/seed_${global_seed}/${misc.timestamp} global_seed=${global_seed}
 # LwF
-clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/lwf_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/lwf/seed_${global_seed} global_seed=${global_seed}
+clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/lwf_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/lwf/seed_${global_seed}/${misc.timestamp} global_seed=${global_seed}
 # HAT
-clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/hat_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/hat/seed_${global_seed} global_seed=${global_seed}
+clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/hat_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/hat/seed_${global_seed}/${misc.timestamp} global_seed=${global_seed}
 # AdaHAT
-clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/adahat_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/adahat/seed_${global_seed} global_seed=${global_seed}
+clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/adahat_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/adahat/seed_${global_seed}/${misc.timestamp} global_seed=${global_seed}
 # FG-AdaHAT (IG)
-clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_ig_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_ig/seed_${global_seed} global_seed=${global_seed}
+clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_ig_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_ig/seed_${global_seed}/${misc.timestamp} global_seed=${global_seed}
 # FG-AdaHAT (OG)
-clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_og_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_og/seed_${global_seed} global_seed=${global_seed}
+clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_og_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_og/seed_${global_seed}/${misc.timestamp} global_seed=${global_seed}
 # FG-AdaHAT (CU)
-clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_cu_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_cu/seed_${global_seed} global_seed=${global_seed}
+clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_cu_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_cu/seed_${global_seed}/${misc.timestamp} global_seed=${global_seed}
 # FG-AdaHAT (ICU)
-clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_icu_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_icu/seed_${global_seed} global_seed=${global_seed}
+clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_icu_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_icu/seed_${global_seed}/${misc.timestamp} global_seed=${global_seed}
 # FG-AdaHAT (AFI)
-clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_afi_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_afi/seed_${global_seed} global_seed=${global_seed}
+clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_afi_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_afi/seed_${global_seed}/${misc.timestamp} global_seed=${global_seed}
 # FG-AdaHAT (FA)
-clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_fa_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_fa/seed_${global_seed} global_seed=${global_seed}
+clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_fa_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_fa/seed_${global_seed}/${misc.timestamp} global_seed=${global_seed}
 # FG-AdaHAT (II)
-clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_ii_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_ii/seed_${global_seed} global_seed=${global_seed}
+clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_ii_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_ii/seed_${global_seed}/${misc.timestamp} global_seed=${global_seed}
 # FG-AdaHAT (GS)
-clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_gs_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_gs/seed_${global_seed} global_seed=${global_seed}
+clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_gs_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_gs/seed_${global_seed}/${misc.timestamp} global_seed=${global_seed}
 # FG-AdaHAT (DL)
-clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_dl_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_dl/seed_${global_seed} global_seed=${global_seed}
+clarena pipeline=CL_MAIN_EXPR index=fgadahat_paper/fgadahat_dl_pmnist_20tasks cl_paradigm=TIL output_dir=output_fgadahat_paper/main_study/pmnist_20tasks_til/fgadahat_dl/seed_${global_seed}/${misc.timestamp} global_seed=${global_seed}
 done
